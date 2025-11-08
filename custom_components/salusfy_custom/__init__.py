0aef3dc6f385989f80506e11e1595bba705a864d
Aldec60 <102868570+Aldec60@users.noreply.github.com>
2025-11-08 09:00:43 +0100

    Implement async setup for Salus IT500 integration

--- custom_components/salusfy_custom/__init__.py
+++ custom_components/salusfy_custom/__init__.py
@@ -1,1 +1,7 @@
+"""Salus IT500 Custom Fork integration."""
+DOMAIN = "salusfy_custom"
 
+async def async_setup(hass, config):
+    """Set up the Salus IT500 Custom integration."""
+    hass.data[DOMAIN] = {}
+    return True
